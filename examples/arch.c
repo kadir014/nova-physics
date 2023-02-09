8c6554161e9c0e72ddc3a832c686c49b22e3f7e8
Kadir Aksoy <46996028+kadir014@users.noreply.github.com>
2023-02-09 04:34:14 +0300

    Fix arch example's title

--- examples/arch.c
+++ examples/arch.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
     // Create example
     Example *example = Example_new(
         1280, 720,
-        "Nova Physics — Playground Example",
+        "Nova Physics — Arch Example",
         165.0,
         1.0/60.0
     );
