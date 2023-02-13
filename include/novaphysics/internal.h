44515f98bbd2ae493fa9b4ef46e688891bcd4ffe
Kadir Aksoy <46996028+kadir014@users.noreply.github.com>
2023-02-13 13:22:50 +0300

    NV_NEW macro

--- include/novaphysics/internal.h
+++ include/novaphysics/internal.h
@@ -11,16 +11,22 @@
 #ifndef NOVAPHYSICS_TYPES_H
 #define NOVAPHYSICS_TYPES_H
 
+#include <stdlib.h>
 #include "novaphysics/vector.h"
 
 
 /**
  * types.h
  * 
- * Nova Physics types & struct forward declarations to prevent circular includes
+ * Nova Physics utility functions and struct forward declarations
+ * to prevent circular includes
  */
 
 struct _nv_Space;
+
+
+// Utility macro to create objects on HEAP
+#define NV_NEW(type) ((type *)malloc(sizeof(type)))
 
 
 #endif
