8716f0ed064a8c27cfe9116ade4848cc02d3be98
Kadir Aksoy <46996028+kadir014@users.noreply.github.com>
2023-02-13 13:22:32 +0300

    Create nv_Array

---
+++ include/novaphysics/array.h
@@ -0,0 +1,94 @@
+/*
+
+  This file is a part of the Nova Physics Engine
+  project and distributed under the MIT license.
+
+  Copyright © Kadir Aksoy
+  https://github.com/kadir014/nova-physics
+
+*/
+
+#ifndef NOVAPHYSICS_ARRAY_H
+#define NOVAPHYSICS_ARRAY_H
+
+#include <stdlib.h>
+
+
+/**
+ * array.h
+ * 
+ * Generic dynamic array stored in HEAP
+ */
+
+
+/**
+ * @brief Generic array
+ * 
+ * @param size Length of the array
+ * @param max Maximum size the array ever reached
+ * @param data Array of void pointers
+ */
+typedef struct {
+    size_t size;
+    size_t max;
+    void **data;
+} nv_Array;
+
+/**
+ * @brief Create new array
+ * 
+ * @return nv_Array * 
+ */
+nv_Array *nv_Array_new();
+
+/**
+ * @brief Free array
+ * 
+ * @param array Array to free
+ */
+void nv_Array_free(nv_Array *array);
+
+/**
+ * @brief Call free function of each element
+ * 
+ * @param array Array
+ * @param free_func Free function
+ */
+void nv_Array_free_each(nv_Array *array, void (free_func)(void *));
+
+/**
+ * @brief Add new element to array
+ * 
+ * @param array Array to append to
+ * @param elem Void pointer to element
+ */
+void nv_Array_add(nv_Array *array, void *elem);
+
+/**
+ * @brief Remove element by index from array and return the element
+ *        Returns NULL if failed
+ * 
+ *        WARNING: The array is not sorted, meaning the array gets
+ *                 slightly randomized every remove call
+ * 
+ * @param array Array
+ * @param index Index of element to remove
+ * @return void *
+ */
+void *nv_Array_pop(nv_Array *array, size_t index);
+
+/**
+ * @brief Remove element from array and return the index
+ *        Returns -1 if failed
+ * 
+ *        WARNING: The array is not sorted, meaning the array gets
+ *                 slightly randomized every remove call
+ * 
+ * @param array Array
+ * @param elem Element to remove
+ * @return void *
+ */
+size_t nv_Array_remove(nv_Array *array, void *elem);
+
+
+#endif
---
+++ src/array.c
@@ -0,0 +1,79 @@
+/*
+
+  This file is a part of the Nova Physics Engine
+  project and distributed under the MIT license.
+
+  Copyright © Kadir Aksoy
+  https://github.com/kadir014/nova-physics
+
+*/
+
+#include "novaphysics/array.h"
+
+
+nv_Array *nv_Array_new() {
+    nv_Array *array = (nv_Array *)malloc(sizeof(nv_Array));
+
+    array->size = 0;
+    array->max = 0;
+    array->data = (void **)malloc(sizeof(void *));
+
+    return array;
+}
+
+void nv_Array_free(nv_Array *array) {
+    free(array->data);
+    array->data = NULL;
+    array->size = 0;
+    free(array);
+}
+
+void nv_Array_free_each(nv_Array *array, void (free_func)(void *)) {
+    for (size_t i = 0; i < array->size; i++)
+        free_func(array->data[i]);
+}
+
+void nv_Array_add(nv_Array *array, void *elem) {
+    // Only reallocate when max capacity is reached
+    if (array->size == array->max) {
+        array->size++;
+        array->max++;
+        array->data = (void **)realloc(array->data, array->size * sizeof(void *));
+    }
+    else {
+        array->size++;
+    }
+
+    array->data[array->size - 1] = elem;
+}
+
+void *nv_Array_pop(nv_Array *array, size_t index) {
+    for (size_t i = 0; i < array->size; i++) {
+        if (i == index) {
+            array->size--;
+            void *elem = array->data[i];
+
+            array->data[i] = array->data[array->size];
+            array->data[array->size] = NULL;
+
+            return elem;
+        }
+    }
+
+    return NULL;
+}
+
+size_t nv_Array_remove(nv_Array *array, void *elem) {
+    for (size_t i = 0; i < array->size; i++) {
+        if (array->data[i] == elem) {
+            array->size--;
+
+            array->data[i] = array->data[array->size];
+            array->data[array->size] = NULL;
+
+            return i;
+        }
+    }
+
+    return -1;
+}
