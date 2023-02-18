2bd8c9d32ac3cb02db15a25023b54d2d26e630d3
Kadir Aksoy <46996028+kadir014@users.noreply.github.com>
2023-02-18 18:29:18 +0300

    Include `material.h` and `constraint.h`

--- include/novaphysics/novaphysics.h
+++ include/novaphysics/novaphysics.h
@@ -36,14 +36,16 @@
 
 #include "novaphysics/vector.h"
 #include "novaphysics/math.h"
+#include "novaphysics/aabb.h"
 #include "novaphysics/constants.h"
 #include "novaphysics/space.h"
 #include "novaphysics/body.h"
+#include "novaphysics/material.h"
+#include "novaphysics/constraint.h"
 #include "novaphysics/resolution.h"
 #include "novaphysics/collision.h"
 #include "novaphysics/contact.h"
 #include "novaphysics/solver.h"
-#include "novaphysics/aabb.h"
 
 
 #endif
