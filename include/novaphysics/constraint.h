866059b31ae301402020bad42a4f7a81afe7a4eb
Kadir Aksoy <46996028+kadir014@users.noreply.github.com>
2023-02-23 20:05:25 +0300

    Move `nv_Spring` head in

--- include/novaphysics/constraint.h
+++ include/novaphysics/constraint.h
@@ -12,7 +12,6 @@
 #define NOVAPHYSICS_CONSTRAINT_H
 
 #include "novaphysics/body.h"
-#include "novaphysics/spring.h"
 
 
 /**
@@ -23,7 +22,7 @@
 
 
 /**
- * @brief Constraint types included in Nova Physics Engine
+ * @brief Constraint types
  * 
  */
 typedef enum {
@@ -55,6 +54,23 @@ void nv_Constraint_free(void *cons);
 
 
 // Different constraint allocator & initializer helpers
+
+/**
+ * @brief Spring constraint head
+ * 
+ * @param length Length of the spring
+ * @param stiffness Stiffnes (strength) of the spring
+ * @param damping TODO
+ * @param achor_a Local anchor point on body A
+ * @param anchor_b Local anchor point on body B
+ */
+typedef struct {
+    double length;
+    double stiffness;
+    double damping;
+    nv_Vector2 anchor_a;
+    nv_Vector2 anchor_b;
+} nv_Spring;
 
 /**
  * @brief Create a new spring constraint
