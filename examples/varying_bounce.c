dec14af7b62b0515104b646bd54a8627aa3094b8
Kadir Aksoy <46996028+kadir014@users.noreply.github.com>
2023-02-28 00:54:34 +0300

    Rename `varying_restitution.c`

--- examples/varying_bounce.c
+++ examples/varying_bounce.c
@@ -12,35 +12,40 @@
 
 
 void setup(Example *example) {
+    nv_Material ground_mat = {
+        .density = 3.0,
+        .restitution = 1.0,
+        .friction = 0.0
+    };
+
     // Create ground 
     nv_Body *ground = nv_Rect_new(
         nv_BodyType_STATIC,
         (nv_Vector2){64.0, 62.5},
         0.0,
-        nv_Material_CONCRETE,
+        ground_mat,
         185.0, 5.0
     );
 
     nv_Space_add(example->space, ground);
 
-    nv_Material basic_material = {
-        .density = 2.0,
-        .restitution = 1.0,
-        .static_friction = nv_Material_WOOD.static_friction,
-        .dynamic_friction = nv_Material_WOOD.dynamic_friction
-    };
+    for (size_t i = 0; i < 5; i++) {
+        nv_Material material = {
+            .density = 1.0,
+            .restitution = (double)i / (5.0 / 6.0) / 5.0,
+            .friction = 0.0
+        };
 
-    nv_Body *box1 = nv_Rect_new(
-        nv_BodyType_DYNAMIC,
-        NV_VEC2(20.0, 20.0),
-        0.0,
-        basic_material,
-        5.0, 5.0
-    );
+        nv_Body *ball = nv_Circle_new(
+            nv_BodyType_DYNAMIC,
+            NV_VEC2(45.0 + (i * (8.0 + 1.0)), 20.0),
+            0.0,
+            material,
+            4.0
+        );
 
-    nv_Space_add(example->space, box1);
-
-    //nv_Body_apply_force(box1, NV_VEC2(0.0, 5.0 * 10e3 * example->sliders[2]->value));
+        nv_Space_add(example->space, ball);
+    }
 }
 
 
@@ -48,7 +53,7 @@ int main(int argc, char *argv[]) {
     // Create example
     Example *example = Example_new(
         1280, 720,
-        "Nova Physics — Stacking Example",
+        "Nova Physics — Varying Restitution Example",
         165.0,
         1.0/60.0,
         ExampleTheme_DARK
