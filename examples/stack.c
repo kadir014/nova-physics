1557692bc3805381625307bd7bdc70e1f98e8fd5
Kadir Aksoy <46996028+kadir014@users.noreply.github.com>
2023-03-01 23:04:25 +0300

    Update stack.c

--- examples/stack.c
+++ examples/stack.c
@@ -25,26 +25,31 @@ void setup(Example *example) {
 
     // Some basic material with no restitution (inelastic)
     nv_Material basic_material = {
-        .density = 1.0,
+        .density = 2.0,
         .restitution = 0.0,
         .friction = nv_Material_WOOD.friction,
     };
 
-    // Create stacking boxes
+    // Create stacked boxes
 
-    double size = 4.0; // Size of the boxes
+    int cols = 6; // Columns of the stack
+    int rows = 17; // Rows of the stack
+    double size = 3.25; // Size of the boxes
     double s2 = size / 2.0;
+    double random_offset = 0.75; // Random horizontal offset magnitude
 
-    for (size_t y = 0; y < 9; y++) {
+    for (size_t y = 0; y < rows; y++) {
     
-        double offset = frand(-0.9, 0.9); // Random horizontal offset
+        double offset = 0;
+        if (random_offset > 0.0)
+            offset = frand(-random_offset / 2.0, random_offset / 2.0);
 
-        for (size_t x = 0; x < 9; x ++) {
+        for (size_t x = 0; x < cols; x ++) {
 
             nv_Body *box = nv_Rect_new(
                 nv_BodyType_DYNAMIC,
                 NV_VEC2(
-                    example->width / 20.0 - s2 + size * x + offset,
+                    example->width / 20.0 - ((double)cols * size) / 2.0 + s2 + size * x + offset,
                     62.5 - 2.5 - s2 - y * size
                 ),
                 0.0,
@@ -54,6 +59,33 @@ void setup(Example *example) {
 
             nv_Space_add(example->space, box);
         }
+    }
+}
+
+
+void update(Example *example) {
+    if (example->counter < 5) return;
+    else example->counter = 0;
+
+    if (example->keys[SDL_SCANCODE_SPACE]) {
+        nv_Vector2 mouse = NV_VEC2(example->mouse.px, example->mouse.py);
+        nv_Vector2 delta = nv_Vector2_sub(NV_VEC2(64.0, mouse.y), mouse);
+        nv_Vector2 dir = nv_Vector2_normalize(delta);
+        nv_Vector2 pos = mouse;
+
+        nv_Body *ball = nv_Circle_new(
+            nv_BodyType_DYNAMIC,
+            pos,
+            0.0,
+            (nv_Material){1.5, 0.3, 0.5},
+            1.5
+        );
+
+        nv_Space_add(example->space, ball);
+
+        nv_Vector2 force = nv_Vector2_muls(dir, 15.0 * 10e3);
+
+        nv_Body_apply_force(ball, force);
     }
 }
 
@@ -70,6 +102,7 @@ int main(int argc, char *argv[]) {
 
     // Set callbacks
     example->setup_callback = setup;
+    example->update_callback = update;
 
     // Run the example
     Example_run(example);
