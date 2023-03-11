6321e846f79c6616254cbbcda7ab84d04fdae085
Kadir Aksoy <46996028+kadir014@users.noreply.github.com>
2023-03-11 15:07:30 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,10 @@
+# The master toctree document.
+master_doc = "index"
+
+# General information about the project.
+project = "Nova Physics"
+copyright = "2023, Kadir Aksoy"
+author = "Kadir Aksoy"
+
+# The theme to use for HTML and HTML Help pages.
 html_theme = "furo"
