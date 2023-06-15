fab8c73f9ede2ac33686d47c35b931ede5478472
Kadir Aksoy <46996028+kadir014@users.noreply.github.com>
2023-06-15 17:46:30 +0300

    rewrite entries

--- src/hashmap.c
+++ src/hashmap.c
@@ -17,10 +17,12 @@
 /**
  * @file hashmap.c
  * 
- * @details Hash map implementation specifically designed to
- *          store {uint32 -> void *} pairs.
+ * @brief Hash map struct and its methods.
  * 
- *          Function documentations are in novaphysics/hashmap.h
+ * Notes:
+ * - This hash map is designed to store {uint32 -> void *} pairs.
+ * - x & (n - 1) is faster than x % n if n is a power of 2.
+ * - Empty keys are indicated with -1 (Since keys are unsigned they wrap).
  */
 
 
@@ -39,6 +41,8 @@ nv_HashMap *nv_HashMap_new() {
     for (size_t i = 0; i < hashmap->capacity; i++)
         hashmap->entries[i].key = -1;
 
+    hashmap->_iter_entries = nv_Array_new();
+
     return hashmap;
 }
 
@@ -53,38 +57,46 @@ void nv_HashMap_free(nv_HashMap *hashmap, void (free_func)(void *)) {
 }
 
 void *nv_HashMap_get(nv_HashMap *hashmap, uint32_t key) {
-    // & hash with capacity-1 to ensure it's within entries array
     uint32_t h = nv_hash(key);
     size_t index = (size_t)(h & (size_t)(hashmap->capacity - 1));
 
+    size_t loop = 0;
     while (hashmap->entries[index].key != -1) {
+        //printf("key: %u %u\n", key, hashmap->entries[index].key);
         // Found key
-        if (key == hashmap->entries[index].key)
+        if (key == hashmap->entries[index].key) {
+            //if (loop > 5) printf("get loop: %zu ", loop);
             return hashmap->entries[index].value;
+        }
 
         // Move to next bucket and wrap around if needed (linear probing)
         index++;
         if (index >= hashmap->capacity) index = 0;
+        loop++;
     }
 
+    //if (loop > 5) printf("get loop: %zu ", loop);
     return NULL;
 }
 
 static uint32_t nv_HashMap_set_entry(
+    nv_HashMap *hashmap,
     nv_HashMapEntry *entries,
     size_t capacity,
     uint32_t key,
     void *value,
     size_t *size_out
 ) {
-    // & hash with capacity-1 to ensure it's within entries array
     uint64_t hash = nv_hash(key);
     size_t index = (size_t)(hash & (size_t)(capacity - 1));
 
+    //printf("set hash: %zu\n", hash);
+    //printf("set hash index: %zu\n", index);
+
     // Loop till we find an empty entry
     while (entries[index].key != -1) {
+        //printf("i: %zu, key: %lu %lu   ", index, entries[index].key, key);
         if (key == entries[index].key) {
-            // Found key (it already exists), update value.
             entries[index].value = value;
             return entries[index].key;
         }
@@ -95,18 +107,48 @@ static uint32_t nv_HashMap_set_entry(
     }
 
     // Didn't find key, insert it.
-    (*size_out)++;
+    if (size_out != NULL)
+        (*size_out)++;
 
     entries[index].key = key;
     entries[index].value = value;
 
+    // nv_HashMapIteratorItem *iter_item = NV_NEW(nv_HashMapIteratorItem);
+    // iter_item->entry = &entries[index];
+    // nv_Array_add(hashmap->_iter_entries, iter_item);
+
     return key;
 }
 
+bool expand(nv_HashMap *hashmap) {
+    size_t new_capacity = hashmap->capacity * 2;
+    // TODO: handle overflow
+
+    nv_HashMapEntry *new_entries = calloc(new_capacity, sizeof(nv_HashMapEntry));
+
+    for (size_t i = 0; i < new_capacity; i++)
+        new_entries[i].key = -1;
+
+    for (size_t i = 0; i < hashmap->capacity; i++) {
+        nv_HashMapEntry entry = hashmap->entries[i];
+        if (entry.key != -1) {
+            nv_HashMap_set_entry(hashmap, new_entries, new_capacity, entry.key, entry.value, NULL);
+        }
+    }
+
+    free(hashmap->entries);
+    hashmap->entries = new_entries;
+    hashmap->capacity = new_capacity;
+    return true;
+}
+
 uint32_t nv_HashMap_set(nv_HashMap *hashmap, uint32_t key, void *value) {
-    // TODO: capacity overflow checks and map expansion
+    if (hashmap->size >= hashmap->capacity / 2) {
+        expand(hashmap);
+    }
 
     return nv_HashMap_set_entry(
+        hashmap,
         hashmap->entries,
         hashmap->capacity,
         key, value,
@@ -119,7 +161,6 @@ void nv_HashMap_remove(
     uint32_t key,
     void (free_func)(void *)
 ) {
-    // & hash with capacity-1 to ensure it's within entries array
     uint32_t h = nv_hash(key);
     size_t i = (size_t)(h & (size_t)(hashmap->capacity - 1));
 
@@ -131,6 +172,13 @@ void nv_HashMap_remove(
                 free_func(hashmap->entries[i].value);
             hashmap->entries[i].value = NULL;
             hashmap->size--;
+
+            // for (size_t j = 0; j < hashmap->_iter_entries->size; j++) {
+            //     if (hashmap->entries->key == ((nv_HashMapIteratorItem *)hashmap->_iter_entries->data[j])->entry->key) {
+            //         free(nv_Array_pop(hashmap->_iter_entries, j));
+            //         break;
+            //     }
+            // }
         }
 
         // Move to next bucket and wrap around if needed (linear probing)
@@ -148,6 +196,9 @@ void nv_HashMap_clear(nv_HashMap *hashmap, void (free_func)(void *)) {
             hashmap->entries[i].key = -1;
         }
     }
+
+    // for (size_t i = 0; i < hashmap->_iter_entries->size; i++)
+    //     free(nv_Array_pop(hashmap->_iter_entries, 0));
 
     hashmap->size = 0;
 }
@@ -181,4 +232,21 @@ bool nv_HashMapIterator_next(nv_HashMapIterator *iterator) {
     }
 
     return false;
+
+    // while (iterator->_index < hashmap->_iter_entries->size) {
+    //     size_t i = iterator->_index++;
+
+    //     nv_HashMapEntry *entry = ((nv_HashMapIteratorItem *)hashmap->_iter_entries->data[i])->entry;
+
+    //     // TODO
+    //     void *value = entry->value;
+    //     if (value == NULL) continue;
+
+    //     iterator->key = entry->key;
+    //     iterator->value = entry->value;
+
+    //     return true;
+    // }
+
+    // return false;
 }
